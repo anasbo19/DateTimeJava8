0da06515015686941a503d77a4e296c26400d880
Anas BOUDI <anasbo2016@gmail.com>
2020-01-25 11:41:51 +0100

    java 7 date time creation done

--- src/com/datetime/example/DateTimeExample.java
+++ src/com/datetime/example/DateTimeExample.java
@@ -1,9 +1,25 @@
 package com.datetime.example;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class DateTimeExample {
 
 	public static void main(String[] args) {
-
+		
+		System.out.println("----------- JAVA 7 DateTime Operations ---------------");
+		System.out.println("Creating an object with current date and time : ");
+		Date currentDateJava7= new Date();
+		System.out.println(currentDateJava7);
+		System.out.println("--------------------------------------------------");
+		
+		System.out.println("Creating an object representing 25 January 2020 : ");
+		Calendar c = Calendar.getInstance();
+		c.set(2020, Calendar.JANUARY, 25, 0, 0, 0);
+		Date jan252020Java7 = c.getTime();
+		System.out.println(jan252020Java7);
+		System.out.println("--------------------------------------------------");
+		
 	}
 
 }
